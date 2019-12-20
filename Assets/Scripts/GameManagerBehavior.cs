65b58a5bdc6c4880ba95362fe20bf7657d7b2b8b
devarifin <51085119+devarifin@users.noreply.github.com>
2019-12-20 20:41:20 +0700

    Update fitur game
    Menambahkan HP pemain

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -7,6 +7,8 @@ public class GameManagerBehavior : MonoBehaviour {
 	public Text goldLabel;
 	private int gold;
 
+	public Text healthLabel;
+	public GameObject[] healthIndicator;
 	void Start(){
 		Gold = 500;
 	}
@@ -21,4 +23,32 @@ public class GameManagerBehavior : MonoBehaviour {
 		}
 	}
 	
+	private int health;
+	public int Health{
+		get{
+			return health;
+		}
+		set{
+			if(value < health){
+				Camera.main.GetComponent<CameraShake>().Shake();
+			}
+			health = value;
+			healthLabel.text = "HEALTH: " + health;
+
+			if(health <= 0 && !gameOver){
+				gameOver = true;
+				GameObject gameOverText = GameObject.FindGameObjectWithTag("GameOver");
+				gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+			}
+
+			for(int i = 0;i < healthIndicator.Length;i++){
+				if(i < Health){
+					healthIndicator[i].SetActive(true);
+				}
+				else{
+					healthIndicator[i].SetActive(false);
+				}
+			}
+		}
+	}
 }
